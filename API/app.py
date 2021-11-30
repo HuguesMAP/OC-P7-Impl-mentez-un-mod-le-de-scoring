f9c0910c46e7d52154f598c22b90b7955bf733ee
Hugues ANTOINE <hugues.antoine@gmail.com>
2021-12-01 00:49:42 +0100

    Test2 Heroku with simple app.py

--- API/app.py
+++ API/app.py
@@ -13,11 +13,11 @@ app = FastAPI()
 # pickle_in = open('../models/model_classifier.pkl','rb')
 # classifier=pickle.load(pickle_in)
 
-# # load data
-# df_data = pd.read_csv("../X_test.csv")
-# #df_data = df_data.drop(columns=['TARGET'])
+# load data
+df_data = pd.read_csv("../X_test.csv")
+#df_data = df_data.drop(columns=['TARGET'])
 
-# threshold_optimized = 0.318
+threshold_optimized = 0.318
 
 # @app.get('/{customerID}')
 # def prediction(customerID: int):
