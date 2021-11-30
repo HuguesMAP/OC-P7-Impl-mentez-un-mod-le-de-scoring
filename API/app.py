c2167c4da36ca0b9e39a21aa0b3424d9563b3ce4
Hugues ANTOINE <hugues.antoine@gmail.com>
2021-12-01 00:46:52 +0100

    Test Heroku with simple app.py

--- API/app.py
+++ API/app.py
@@ -10,42 +10,47 @@ import pandas as pd
      
 # Create app and model objects
 app = FastAPI()
-pickle_in = open('../models/model_classifier.pkl','rb')
-classifier=pickle.load(pickle_in)
+# pickle_in = open('../models/model_classifier.pkl','rb')
+# classifier=pickle.load(pickle_in)
 
-# load data
-df_data = pd.read_csv("../X_test.csv")
-#df_data = df_data.drop(columns=['TARGET'])
+# # load data
+# df_data = pd.read_csv("../X_test.csv")
+# #df_data = df_data.drop(columns=['TARGET'])
 
-threshold_optimized = 0.318
+# threshold_optimized = 0.318
 
-@app.get('/{customerID}')
-def prediction(customerID: int):
-    if customerID in df_data['SK_ID_CURR'].values:
+# @app.get('/{customerID}')
+# def prediction(customerID: int):
+#     if customerID in df_data['SK_ID_CURR'].values:
     
-        # select datas of the customer
-        customer_data = df_data[df_data['SK_ID_CURR']==customerID]
+#         # select datas of the customer
+#         customer_data = df_data[df_data['SK_ID_CURR']==customerID]
 
-        # calculate the probability with the model for the customer selected
-        predict_proba = classifier.predict_proba(customer_data.values.reshape(1,-1))
+#         # calculate the probability with the model for the customer selected
+#         predict_proba = classifier.predict_proba(customer_data.values.reshape(1,-1))
 
-        # just consider the probability of not repaying a loan
-        predict_proba = predict_proba[:, 1][0]
+#         # just consider the probability of not repaying a loan
+#         predict_proba = predict_proba[:, 1][0]
 
-        # use threshold_optimized for prediction
-        if predict_proba >= threshold_optimized:
-            prediction = 1
-        else:
-            prediction = 0
+#         # use threshold_optimized for prediction
+#         if predict_proba >= threshold_optimized:
+#             prediction = 1
+#         else:
+#             prediction = 0
 
-        return {
-            'CustomerID': customerID,
-            'Prediction': prediction,
-            'Probability': round(predict_proba,4)
+#         return {
+#             'CustomerID': customerID,
+#             'Prediction': prediction,
+#             'Probability': round(predict_proba,4)
 
-        }
-    else:
-        return {f'CustomerID : {customerID} doesn\'t exit'}
+#         }
+#     else:
+#         return {f'CustomerID : {customerID} doesn\'t exit'}
+
+
+@app.get('/')
+def index():
+    return {'message': 'Hello, stranger'}
 
 
 # 4. Run the API with uvicorn
